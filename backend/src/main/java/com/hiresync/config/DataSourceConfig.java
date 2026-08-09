6cd4afc9cdf205761b4f6d04d51f49e642901204
vyshnav-26 <vyshnav4909@gmail.com>
2026-08-09 14:22:50 +0530

    Comment out DataSourceConfig class and its methods. Reason:- it's for running locally

--- backend/src/main/java/com/hiresync/config/DataSourceConfig.java
+++ backend/src/main/java/com/hiresync/config/DataSourceConfig.java
@@ -1,44 +1,44 @@
-package com.hiresync.config;
+// package com.hiresync.config;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.scheduling.annotation.EnableScheduling;
+// import com.zaxxer.hikari.HikariDataSource;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.boot.jdbc.DataSourceBuilder;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Primary;
+// import org.springframework.scheduling.annotation.EnableScheduling;
 
-import javax.sql.DataSource;
+// import javax.sql.DataSource;
 
-@Configuration
-@EnableScheduling
-public class DataSourceConfig {
+// @Configuration
+// @EnableScheduling
+// public class DataSourceConfig {
 
-    @Value("${spring.datasource.url}")
-    private String databaseUrl;
+//     @Value("${spring.datasource.url}")
+//     private String databaseUrl;
 
-    @Value("${spring.datasource.username}")
-    private String databaseUsername;
+//     @Value("${spring.datasource.username}")
+//     private String databaseUsername;
 
-    @Value("${spring.datasource.password}")
-    private String databasePassword;
+//     @Value("${spring.datasource.password}")
+//     private String databasePassword;
 
-    @Bean
-    @Primary
-    public DataSource dataSource() {
+//     @Bean
+//     @Primary
+//     public DataSource dataSource() {
 
-        HikariDataSource dataSource = (HikariDataSource) DataSourceBuilder.create()
-                .url(databaseUrl)
-                .username(databaseUsername)
-                .password(databasePassword)
-                .driverClassName("org.postgresql.Driver")
-                .build();
+//         HikariDataSource dataSource = (HikariDataSource) DataSourceBuilder.create()
+//                 .url(databaseUrl)
+//                 .username(databaseUsername)
+//                 .password(databasePassword)
+//                 .driverClassName("org.postgresql.Driver")
+//                 .build();
 
-        dataSource.setConnectionTimeout(10000);
-        dataSource.setValidationTimeout(3000);
-        dataSource.setMaximumPoolSize(10);
-        dataSource.setMinimumIdle(2);
+//         dataSource.setConnectionTimeout(10000);
+//         dataSource.setValidationTimeout(3000);
+//         dataSource.setMaximumPoolSize(10);
+//         dataSource.setMinimumIdle(2);
 
-        return dataSource;
-    }
-}
+//         return dataSource;
+//     }
+// }
